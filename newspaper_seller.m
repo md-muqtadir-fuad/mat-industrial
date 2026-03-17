23ecf35594366fdbc8a902d212067562e757f27f
Md Muqtadir Fuad <mmfuad01@gmail.com>
2026-03-18 02:20:03 +0600

    newspaper problem solved

--- newspaper_seller.m
+++ newspaper_seller.m
@@ -16,6 +16,8 @@ rand_digit_poor = [44, 66, 82, 94, 100]';
 per_day_demands = zeros(total_days, 1);
 %%
 newsday_types = days(discretesample(newsday_type_prob, total_days));
+per_sim_profit = zeros(7, 1);
+simulation = 1;
 
 %per day demand for 20 days
 for i = 1: total_days
@@ -45,40 +47,33 @@ for i = 1: total_days
 end
 
 %% revenue, cost, profit, loss, salvage calculation
-for buy = (40:10:100)
+for bought = (40:10:100)
     per_day_rev = zeros(total_days, 1);
     cost_of_newspaper = zeros(total_days, 1);
     per_day_profit = zeros(total_days, 1);
     per_day_lost_profit = zeros(total_days, 1);
     per_day_salvage = zeros(total_days, 1);
     for i = 1: total_days
-        if buy < per_day_demands(i)
+        if bought < per_day_demands(i)
             per_day_rev(i) = bought * selling_price;
+            per_day_lost_profit(i) = ( per_day_demands(i) - bought)*(selling_price - buying_price);
+            per_day_salvage(i) = 0;
         else
             per_day_rev(i) = per_day_demands(i) * selling_price;
+            per_day_lost_profit(i) = 0;
+            per_day_salvage(i) = ( bought - per_day_demands(i))*scrap_price;
         end
         cost_of_newspaper(i) = buying_price * bought;
+        per_day_profit(i) = per_day_rev(i) - cost_of_newspaper(i) - per_day_lost_profit(i) - per_day_salvage(i);
     end
+    
+    per_sim_profit(simulation) = sum(per_day_profit);
+    simulation = simulation + 1;
 end
 
+[max_profit, max_profit_sim] = max(per_sim_profit);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+final_result = [max_profit, max_profit_sim];
+final_names = {'max_profit', 'max_profit_sim'};
+final_result_table = array2table(final_result, 'variablenames', final_names);
+final_result_table
